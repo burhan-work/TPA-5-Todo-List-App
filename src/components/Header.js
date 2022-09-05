74fd8bab4edd450e57a8df00cd5c62d1ea806f75
burhan-work <burhan.bnw@gmail.com>
2022-09-05 23:24:43 +0800

    finished

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='bg-white text-dark py-2 px-3 text-center rounded mb-5'>
+      <h1>Todo List</h1>
+    </header>
+  )
+}
+
+export default Header
